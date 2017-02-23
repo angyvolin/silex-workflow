aa7f27c15fe50e303944b4542790250425a32dcc
Andrii Volin <angy.volin@gmail.com>
2017-02-23 23:56:14 +0200

    add symfony/workflow service provider

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# silex-workflow
-Silex service provider for symfony/workflow component
+# WorkflowServiceProvider
+
+Silex service provider for [symfony/workflow](https://github.com/symfony/workflow) component.
+
+This version is intended for use with Silex 2.*.
+
+## Installation
+
+`composer require angyvolin/silex-worflow`
+
+> To be able to use twig helpers you also require a [symfony/twig-bridge](https://github.com/symfony/twig-bridge) package:
+>`composer require symfony/twig-bridge`
+
+## Configuration
+
+Register the service and pass workflow configuration:
+
+```php
+<?php
+
+use Angyvolin\Provider\WorkflowServiceProvider;
+
+$app->register(new WorkflowServiceProvider(array(
+    'workflow.config' => $workflowConfig,
+)));
+
+```
+
+## Usage
+
+See official [symfony/workflow documentation](http://symfony.com/doc/current/components/workflow.html) examples.
+
+## Author
+
+* Andrii Volin angy.volin@gmail.com
---
+++ src/Provider/WorkflowServiceProvider.php
@@ -0,0 +1,168 @@
+<?php
+
+namespace Angyvolin\Provider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+use Silex\Api\BootableProviderInterface;
+use Silex\Application;
+use Symfony\Bridge\Twig\Extension\WorkflowExtension;
+use Symfony\Component\PropertyAccess\PropertyAccessorInterface;
+use Symfony\Component\Workflow\Definition;
+use Symfony\Component\Workflow\MarkingStore\MultipleStateMarkingStore;
+use Symfony\Component\Workflow\MarkingStore\SingleStateMarkingStore;
+use Symfony\Component\Workflow\Registry;
+use Symfony\Component\Workflow\StateMachine;
+use Symfony\Component\Workflow\Transition;
+use Symfony\Component\Workflow\Validator\DefinitionValidatorInterface;
+use Symfony\Component\Workflow\Validator\StateMachineValidator;
+use Symfony\Component\Workflow\Validator\WorkflowValidator;
+use Symfony\Component\Workflow\Workflow;
+
+class WorkflowServiceProvider implements ServiceProviderInterface, BootableProviderInterface
+{
+    public function register(Container $app)
+    {
+        $app['workflow.factory'] = $app->protect(function ($def, $name) use ($app) {
+            return new Workflow($def, null, $app['dispatcher'], $name);
+        });
+
+        $app['state_machine.factory'] = $app->protect(function ($definition, $markingStoreDefinition, $name) use ($app) {
+            return new StateMachine($definition, $markingStoreDefinition, $app['dispatcher'], $name);
+        });
+
+        $app['workflow.marking_store.single_state'] = $app->protect(
+            function ($property = 'marking', PropertyAccessorInterface $propertyAccessor = null) {
+                return new SingleStateMarkingStore($property, $propertyAccessor);
+            }
+        );
+
+        $app['workflow.marking_store.multiple_state'] = $app->protect(
+            function ($property = 'marking', PropertyAccessorInterface $propertyAccessor = null) {
+                return new MultipleStateMarkingStore($property, $propertyAccessor);
+            }
+        );
+
+        $app['workflow.registry'] = function () {
+            return new Registry();
+        };
+
+        $app['workflow.config'] = [];
+    }
+
+    public function boot(Application $app)
+    {
+        $this->registerWorkflowConfiguration($app);
+
+        if (class_exists('Symfony\Bridge\Twig\Extension\WorkflowExtension')) {
+            $app->extend('twig', function (\Twig_Environment $twig, Container $app) {
+                $twig->addExtension(new WorkflowExtension($app['workflow.registry']));
+
+                return $twig;
+            });
+        }
+    }
+
+    private function registerWorkflowConfiguration(Container $app)
+    {
+        if (!$workflows = $app['workflow.config']) {
+            return;
+        }
+
+        foreach ($workflows as $name => $workflow) {
+            $type = $workflow['type'];
+
+            if (!isset($workflow['transitions'][0])) {
+                foreach ((array) $workflow['transitions'] as $workflowName => $transition) {
+                    if (array_key_exists('name', $transition)) {
+                        continue;
+                    }
+
+                    $transition['name'] = $workflowName;
+                    $workflow['transitions'][$workflowName] = $transition;
+                }
+            }
+
+            $workflowId = sprintf('%s.%s', $type, $name);
+            $workflowDefinitionId = sprintf('%s.definition', $workflowId);
+            $markingStoreDefinitionId = sprintf('%s.marking_store', $workflowId);
+
+            $app[$workflowDefinitionId] = function () use ($workflow, $type, $name) {
+                $transitions = [];
+
+                foreach ((array) $workflow['transitions'] as $transition) {
+                    if ($type === 'workflow') {
+                        $transitions[] = new Transition($transition['name'], $transition['from'], $transition['to']);
+                    } elseif ($type === 'state_machine') {
+                        foreach ((array) $transition['from'] as $from) {
+                            foreach ((array) $transition['to'] as $to) {
+                                $transitions[] = new Transition($transition['name'], $from, $to);
+                            }
+                        }
+                    }
+                }
+
+                $initialPlace = isset($workflow['initial_place']) ? $workflow['initial_place'] : null;
+
+                return new Definition($workflow['places'], $transitions, $initialPlace);
+            };
+
+            if ($app['debug']) {
+                $this
+                    ->createValidator($type, isset($workflow['marking_store']['type']) ? $workflow['marking_store']['type'] : null)
+                    ->validate($app[$workflowDefinitionId], $name);
+            }
+
+            $app[$markingStoreDefinitionId] = function (Container $app) use ($workflow) {
+                if (isset($workflow['marking_store']['type'])) {
+                    return call_user_func_array(
+                        $app['workflow.marking_store.'.$workflow['marking_store']['type']],
+                        $workflow['marking_store']['arguments']
+                    );
+                }
+
+                if (isset($workflow['marking_store']['service'])) {
+                    return $app[$workflow['marking_store']['service']];
+                }
+
+                return null;
+            };
+
+            $app[$workflowId] = function (Container $app) use ($name, $type, $workflowDefinitionId, $markingStoreDefinitionId) {
+                return call_user_func(
+                    $app[sprintf('%s.factory', $type)],
+                    $app[$workflowDefinitionId],
+                    $app[$markingStoreDefinitionId],
+                    $name
+                );
+            };
+
+            $app->extend('workflow.registry', function (Registry $registry, Container $app) use ($workflow, $workflowId) {
+                foreach ($workflow['supports'] as $supportedClass) {
+                    $registry->add($app[$workflowId], $supportedClass);
+                }
+
+                return $registry;
+            });
+        }
+    }
+
+    /**
+     * @param string $workflowType
+     * @param string $markingStore
+     *
+     * @return DefinitionValidatorInterface
+     */
+    private function createValidator($workflowType, $markingStore)
+    {
+        if ('state_machine' === $workflowType) {
+            return new StateMachineValidator();
+        }
+
+        if ('single_state' === $markingStore) {
+            return new WorkflowValidator(true);
+        }
+
+        return new WorkflowValidator();
+    }
+}
